92356f1370fb35eb8e04af1d3270cd35305aedd4
Steven Robbins <stevether@gmail.com>
2013-02-06 21:27:52 -0800

    using permalinks for traffic examples

--- include/template/traffic/list.php
+++ include/template/traffic/list.php
@@ -1,7 +1,7 @@
 <ul>
 
-    <li><a href="<?=BASE?>traffic/1">List of all browsers and their varients</a></li>
+    <li><a href="<?=BASE?>traffic/list-of-browsers-and-their-varients">List of all browsers and their varients</a></li>
 
-    <li><a href="<?=BASE?>traffic/2">Top pages of all time</a></li>
+    <li><a href="<?=BASE?>traffic/top-page-of-all-time">Top pages of all time</a></li>
 
 </ul>
