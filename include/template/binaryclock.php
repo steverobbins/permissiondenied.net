ce1149d807d8c7996d1107503e6979c1be14ddfb
steverobbins <steven.j.robbins@gmail.com>
2013-02-11 23:33:53 -0800

    basic structure for clock

---
+++ include/template/binaryclock.php
@@ -0,0 +1,93 @@
+<table>
+
+    <tr>
+
+        <td class="empty"></td>
+
+        <td class="hour fourth two"></td>
+
+        <td class="empty"></td>
+
+        <td class="minute fourth two"></td>
+
+        <td class="empty"></td>
+
+        <td class="sec fourth two"></td>
+
+        <td class="hints right"></td>
+
+    </tr>
+
+    <tr>
+
+        <td class="empty"></td>
+
+        <td class="hour third two"></td>
+
+        <td class="minute third one"></td>
+
+        <td class="minute third two"></td>
+
+        <td class="sec third one"></td>
+
+        <td class="sec third two"></td>
+
+        <td class="hints right"></td>
+
+    </tr>
+
+    <tr>
+
+        <td class="hour second one"></td>
+
+        <td class="hour second two"></td>
+
+        <td class="minute second one"></td>
+
+        <td class="minute second two"></td>
+
+        <td class="sec second one"></td>
+
+        <td class="sec second two"></td>
+
+        <td class="hints right"></td>
+
+    </tr>
+
+    <tr>
+
+        <td class="hour first one"></td>
+
+        <td class="hour first two"></td>
+
+        <td class="minute first one"></td>
+
+        <td class="minute first two"></td>
+
+        <td class="sec first one"></td>
+
+        <td class="sec first two"></td>
+
+        <td class="hints right"></td>
+
+    </tr>>
+
+    <tr>
+
+        <td class="hints bottom"></td>
+
+        <td class="hints bottom"></td>
+
+        <td class="hints bottom"></td>
+
+        <td class="hints bottom"></td>
+
+        <td class="hints bottom"></td>
+
+        <td class="hints bottom"></td>
+
+        <td class="time bottom right"></td>
+
+    </tr>
+
+</table>
