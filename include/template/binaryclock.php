ef67e807779278095a4e3b6dab8d71a4d46847ca
steverobbins <steven.j.robbins@gmail.com>
2013-02-11 23:47:50 -0800

    start styling

--- include/template/binaryclock.php
+++ include/template/binaryclock.php
@@ -2,89 +2,159 @@
 
     <tr>
 
+        <td class="space"></td>
+
         <td class="empty"></td>
+
+        <td class="space"></td>
 
         <td class="hour fourth two"></td>
 
+        <td class="space"></td>
+
         <td class="empty"></td>
+
+        <td class="space"></td>
 
         <td class="minute fourth two"></td>
 
+        <td class="space"></td>
+
         <td class="empty"></td>
+
+        <td class="space"></td>
 
         <td class="sec fourth two"></td>
 
-        <td class="hint right"></td>
+        <td class="space"></td>
+
+        <td class="hint right"><p>8</p></td>
 
     </tr>
 
     <tr>
+
+        <td class="space"></td>
 
         <td class="empty"></td>
 
+        <td class="space"></td>
+
         <td class="hour third two"></td>
+
+        <td class="space"></td>
 
         <td class="minute third one"></td>
 
+        <td class="space"></td>
+
         <td class="minute third two"></td>
+
+        <td class="space"></td>
 
         <td class="sec third one"></td>
 
+        <td class="space"></td>
+
         <td class="sec third two"></td>
 
-        <td class="hint right"></td>
+        <td class="space"></td>
+
+        <td class="hint right"><p>4</p></td>
 
     </tr>
 
     <tr>
+
+        <td class="space"></td>
 
         <td class="hour second one"></td>
 
+        <td class="space"></td>
+
         <td class="hour second two"></td>
+
+        <td class="space"></td>
 
         <td class="minute second one"></td>
 
+        <td class="space"></td>
+
         <td class="minute second two"></td>
+
+        <td class="space"></td>
 
         <td class="sec second one"></td>
 
+        <td class="space"></td>
+
         <td class="sec second two"></td>
 
-        <td class="hint right"></td>
+        <td class="space"></td>
+
+        <td class="hint right"><p>2</p></td>
 
     </tr>
 
     <tr>
+
+        <td class="space"></td>
 
         <td class="hour first one"></td>
 
+        <td class="space"></td>
+
         <td class="hour first two"></td>
+
+        <td class="space"></td>
 
         <td class="minute first one"></td>
 
+        <td class="space"></td>
+
         <td class="minute first two"></td>
+
+        <td class="space"></td>
 
         <td class="sec first one"></td>
 
+        <td class="space"></td>
+
         <td class="sec first two"></td>
 
-        <td class="hint right"></td>
+        <td class="space"></td>
+
+        <td class="hint right"><p>1</p></td>
 
     </tr>
 
     <tr>
 
-        <td class="hint bottom"></td>
+        <td class="space"></td>
 
         <td class="hint bottom"></td>
 
-        <td class="hint bottom"></td>
+        <td class="space"></td>
 
         <td class="hint bottom"></td>
 
-        <td class="hint bottom"></td>
+        <td class="space"></td>
 
         <td class="hint bottom"></td>
+
+        <td class="space"></td>
+
+        <td class="hint bottom"></td>
+
+        <td class="space"></td>
+
+        <td class="hint bottom"></td>
+
+        <td class="space"></td>
+
+        <td class="hint bottom"></td>
+
+        <td class="space"></td>
 
         <td class="time bottom right"></td>
 
