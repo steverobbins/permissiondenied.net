a6c37a7fc317a61a468932643329d7411cc97aef
steverobbins <steven.j.robbins@gmail.com>
2013-02-11 23:55:10 -0800

    removed unneccessary styles

--- include/template/binaryclock.php
+++ include/template/binaryclock.php
@@ -2,172 +2,170 @@
 
     <tr>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="empty"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hour fourth two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="empty"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="minute fourth two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="empty"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="sec fourth two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hint right"><p>8</p></td>
 
     </tr>
 
-    <tr><td colspan="14"></td></tr>    
+    <tr><td colspan="14">&nbsp;</td></tr>    
 
     <tr>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="empty"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hour third two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="minute third one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="minute third two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="sec third one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="sec third two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hint right"><p>4</p></td>
 
     </tr>
 
-    <tr><td colspan="14"></td></tr>    
+    <tr><td colspan="14">&nbsp;</td></tr>    
 
     <tr>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hour second one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hour second two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="minute second one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="minute second two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="sec second one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="sec second two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hint right"><p>2</p></td>
 
     </tr>
 
-    <tr><td colspan="14"></td></tr>    
+    <tr><td colspan="14">&nbsp;</td></tr>    
 
     <tr>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hour first one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hour first two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="minute first one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="minute first two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="sec first one"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="sec first two"></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="hint right"><p>1</p></td>
 
     </tr>
 
-    <tr><td colspan="14"></td></tr>    
+    <tr><td colspan="14">&nbsp;</td></tr>    
 
     <tr>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
-        <td class="hint bottom"></td>
+        <td class="hint bottom"><p>h</p></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
-        <td class="hint bottom"></td>
+        <td class="hint bottom"><p>h</p></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
-        <td class="hint bottom"></td>
+        <td class="hint bottom"><p>m</p></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
-        <td class="hint bottom"></td>
+        <td class="hint bottom"><p>m</p></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
-        <td class="hint bottom"></td>
+        <td class="hint bottom"><p>s</p></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
-        <td class="hint bottom"></td>
+        <td class="hint bottom"><p>s</p></td>
 
-        <td class="space"></td>
+        <td>&nbsp;</td>
 
         <td class="time bottom right"></td>
 
     </tr>
-
-    <tr><td colspan="14"></td></tr>    
 
 </table>
