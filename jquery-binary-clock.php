10ae51c39a5f9f1e45d551c5a8b0a9c4af075594
steverobbins <steven.j.robbins@gmail.com>
2013-02-11 23:22:01 -0800

    starting binary clock

---
+++ jquery-binary-clock.php
@@ -0,0 +1,22 @@
+<?php include 'include/global.php' ?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+
+    <head>
+    
+        <title>jQuery Binary Clock - <?=SITE_NAME?></title>
+
+        <link rel="stylesheet" type="text/css" href="<?=BASE?>css/binaryclock.css">
+        
+        <?php include 'include/template/global/head.php' ?>
+
+        <script type="text/javascript" src="<?=BASE?>js/binaryclock.js"></script>
+    
+    </head>
+    
+    <body>
+        
+        <?php include 'include/template/binaryclock.php' ?>
+
+    </body>
+    
+</html>
