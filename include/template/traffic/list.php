c5e0f7be1b62df4dcb87b5a3f931b2cdc2ff95e6
steverobbins <steven.j.robbins@gmail.com>
2013-03-10 21:15:31 -0700

    fixing typo

--- include/template/traffic/list.php
+++ include/template/traffic/list.php
@@ -2,6 +2,6 @@
 
     <li><a href="<?=BASE?>traffic/top-browsers-and-their-varients">Top browsers and their varients</a></li>
 
-    <li><a href="<?=BASE?>traffic/top-page-of-all-time">Top pages of all time</a></li>
+    <li><a href="<?=BASE?>traffic/top-pages-of-all-time">Top pages of all time</a></li>
 
 </ul>
