eb97f55981e586c92026f12c77efc612648deb9e
Steve Robbins <steven.j.robbins@gmail.com>
2014-03-16 04:09:25 -0700

    identicon fix

--- identicon.php
+++ identicon.php
@@ -1,68 +1,68 @@
 <?php
 
-    include 'include/global.php';    
-    
+    include 'include/global.php';
+
     if (isset($_GET['hashText'])) {
-        
+
         header("Location: " . BASE . "identicon/" . (!empty($_GET['imageSize']) ? ((int)$_GET['imageSize'] <= 0 ? 32 : $_GET['imageSize']) : 32) . "/" . (isset($_GET['hashValue']) ? md5($_GET['hashText']) : $_GET['hashText']) . '.png');
     }
 
     if (isset($_GET['hash'])) {
-    
+
         header("Content-Type: image/png");
-            
+
         header("Cache-Control: private, max-age=31449600, pre-check=31449600");
         header("Pragma: private");
         header("Expires: " . date(DATE_RFC822, strtotime("2 day")));
-        
+
         if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
-            
+
             header('Last-Modified: ' . $_SERVER['HTTP_IF_MODIFIED_SINCE'], true, 304);
             exit;
         }
-            
+
         header('Last-Modified: ' . date("m/j/y h:i", time()));
-        
+
         include 'include/function.identicon.php';
-        
-        identicon($_GET['hash'], (isset($_GET['size']) ? ((int)$_GET['size'] > 600 ? 600 : ((int)$_GET['size'] <= 0 ? 32 : $_GET['size'])) : 32));        
+
+        identicon($_GET['hash'], (isset($_GET['size']) ? ((int)$_GET['size'] > 600 ? 600 : ((int)$_GET['size'] <= 0 ? 32 : $_GET['size'])) : 32));
     }
-    
+
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 
     <head>
-    
+
         <title>PHP Identicon Generator - <?php echo SITE_NAME ?></title>
-        
+
         <style type="text/css">
-        
+
             #body {
                 width: 588px;
             }
-        
+
         </style>
-        
+
         <?php template('global/head') ?>
-        
+
         <meta name="description" content="A simple API to create an identicon bashed on a hash string." />
-        
+
         <meta name="keywords" content="identicon, hash, md5, creator, api, pixel, size, image, png" />
-    
+
     </head>
-    
+
     <body>
-        
+
         <?php template('global/header') ?>
-        
-        <?php template('global/side', array('code-samples' => $codesamples) ?>
-        
+
+        <?php template('global/side', array('code-samples' => $codesamples)) ?>
+
         <div id="body"><?php template('identicon') ?></div>
-        
+
         <div class="clear"></div>
-    
+
         <?php template('global/footer') ?>
-        
+
     </body>
-    
+
 </html>
--- include/function.identicon.php
+++ include/function.identicon.php
@@ -1,37 +1,37 @@
 <?php
-    
+
     function identicon($stringToHash, $size = 32, $alt = '') {
-        
+
         $hash = $stringToHash;
-        
+
         $csh=hexdec(substr($hash,0,1)); // corner sprite shape
         $ssh=hexdec(substr($hash,1,1)); // side sprite shape
         $xsh=hexdec(substr($hash,2,1))&7; // center sprite shape
-        
+
         $cro=hexdec(substr($hash,3,1))&3; // corner sprite rotation
         $sro=hexdec(substr($hash,4,1))&3; // side sprite rotation
         $xbg=hexdec(substr($hash,5,1))%2; // center sprite background
-        
+
         /* corner sprite foreground color */
         $cfr=hexdec(substr($hash,6,2));
         $cfg=hexdec(substr($hash,8,2));
         $cfb=hexdec(substr($hash,10,2));
-        
+
         /* side sprite foreground color */
         $sfr=hexdec(substr($hash,12,2));
         $sfg=hexdec(substr($hash,14,2));
         $sfb=hexdec(substr($hash,16,2));
-        
+
         /* final angle of rotation */
         $angle=hexdec(substr($hash,18,2));
-        
+
         /* size of each sprite */
         $spriteZ=128;
-        
+
         /* start with blank 3x3 identicon */
         $identicon=imagecreatetruecolor($spriteZ*3,$spriteZ*3);
         imageantialias($identicon,TRUE);
-        
+
         /* assign white as background */
         $bg=imagecolorallocate($identicon,255,255,255);
         //imagefilledrectangle($identicon,0,0,$spriteZ,$spriteZ,$bg);
@@ -39,7 +39,7 @@
         imagesavealpha($identicon, true);
         $transparent = imagecolorallocatealpha($identicon, 255, 255, 255, 127);
         imagefilledrectangle($identicon, 0, 0, $spriteZ, $spriteZ, $transparent);
-        
+
         /* generate corner sprites */
         $corner=getsprite($spriteZ,$csh,$cfr,$cfg,$cfb,$cro);
         imagecopy($identicon,$corner,0,0,0,0,$spriteZ,$spriteZ);
@@ -49,7 +49,7 @@
         imagecopy($identicon,$corner,$spriteZ*2,$spriteZ*2,0,0,$spriteZ,$spriteZ);
         $corner=imagerotate($corner,90,$bg);
         imagecopy($identicon,$corner,$spriteZ*2,0,0,0,$spriteZ,$spriteZ);
-        
+
         /* generate side sprites */
         $side=getsprite($spriteZ,$ssh,$sfr,$sfg,$sfb,$sro);
         imagecopy($identicon,$side,$spriteZ,0,0,0,$spriteZ,$spriteZ);
@@ -59,20 +59,20 @@
         imagecopy($identicon,$side,$spriteZ,$spriteZ*2,0,0,$spriteZ,$spriteZ);
         $side=imagerotate($side,90,$bg);
         imagecopy($identicon,$side,$spriteZ*2,$spriteZ,0,0,$spriteZ,$spriteZ);
-        
+
         /* generate center sprite */
         $center=getcenter($spriteZ,$xsh,$cfr,$cfg,$cfb,$sfr,$sfg,$sfb,$xbg);
         imagecopy($identicon,$center,$spriteZ,$spriteZ,0,0,$spriteZ,$spriteZ);
-        
+
         // $identicon=imagerotate($identicon,$angle,$bg);
-        
+
         /* make white transparent */
         imagecolortransparent($identicon,$bg);
-        
+
         /* create blank image according to specified dimensions */
         $resized=imagecreatetruecolor($size,$size);
         imageantialias($resized,TRUE);
-        
+
         /* assign white as background */
         $bg=imagecolorallocate($identicon,255,255,255);
         //imagefilledrectangle($identicon,0,0,$spriteZ,$spriteZ,$bg);
@@ -80,22 +80,22 @@
         imagesavealpha($resized, true);
         $transparent = imagecolorallocatealpha($resized, 255, 255, 255, 127);
         imagefilledrectangle($resized, 0, 0, $size, $size, $transparent);
-        
+
         /* resize identicon according to specification */
         imagecopyresampled($resized,$identicon,0,0,(imagesx($identicon)-$spriteZ*3)/2,(imagesx($identicon)-$spriteZ*3)/2,$size,$size,$spriteZ*3,$spriteZ*3);
-        
+
         /* make white transparent */
         imagecolortransparent($resized,$bg);
-        
+
         //ob_start();
         imagepng($resized);
         //$image = ob_get_clean();
-        
+
         /*return '<img src="data:image/png;base64,' . base64_encode($image) .
             '" alt="' . $alt .
             '"' . (!empty($alt) ? ' title="' . $alt . '"' : '') .
             ' width="' . $size . '" height="' . $size . '" />';*/
-            
+
         imagedestroy($resized);
     }
 
@@ -103,8 +103,8 @@
         $sprite=imagecreatetruecolor($spriteZ,$spriteZ);
         imageantialias($sprite,TRUE);
         $fg=imagecolorallocate($sprite,$R,$G,$B);
-        //$bg=imagecolorallocate($sprite,255,255,255);
-        //imagefilledrectangle($sprite,0,0,$spriteZ,$spriteZ,$bg);
+        $bg=imagecolorallocate($sprite,255,255,255);
+        imagefilledrectangle($sprite,0,0,$spriteZ,$spriteZ,$bg);
         imagealphablending($sprite, false);
         imagesavealpha($sprite, true);
         $transparent = imagecolorallocatealpha($sprite, 255, 255, 255, 127);
@@ -286,7 +286,7 @@
             $sprite=imagerotate($sprite,90,$bg);
         return $sprite;
     }
-    
+
     /* generate sprite for center block */
     function getcenter($spriteZ,$shape,$fR,$fG,$fB,$bR,$bG,$bB,$usebg) {
         $sprite=imagecreatetruecolor($spriteZ,$spriteZ);
@@ -402,5 +402,5 @@
             imagefilledpolygon($sprite,$shape,count($shape)/2,$fg);
         return $sprite;
     }
-    
+
 ?>
